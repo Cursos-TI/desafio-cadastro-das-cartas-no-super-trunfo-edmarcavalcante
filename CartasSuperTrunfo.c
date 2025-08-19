e49c99fdacf66225b5d40d2167396ebdee0df369
EDMAR ALMEIDA CAVALCANTE <46653761+edmarcavalcante@users.noreply.github.com>
2025-08-19 10:57:28 +0000

    Commit nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,8 +12,16 @@ int main() {
     float area1, area2;
     float pib1, pib2;
     int num_pontos_tur1, num_pontos_tur2;
+    char estado1[2], estado2[2];
+    char codigo1[4], codigo2[4];
     
     printf("DADOS DO 1º ESTADO \n");
+
+    printf("Digite a letra do Estado: \n");
+    scanf("%s", estado1);
+
+    printf("Digite o código da cidade: \n");
+    scanf("%s", codigo1);
 
     printf("Digite a População: \n");
     scanf("%d", &populacao1);
@@ -27,13 +35,27 @@ int main() {
     printf("Digite o Nº de Pontos Turísticos: \n");
     scanf("%d", &num_pontos_tur1);
 
-    printf("Primeiro Estado - População: %d \n", populacao1 );
-    printf("Primeiro Estado - Área: %f \n", area1);
-    printf("Primeiro Estado - PIB: %f \n", pib1);   
-    printf("Primeiro Estado - Nº Pontos Turísticos: %d \n", num_pontos_tur1);   
+    float densidade1 = (float) populacao1 / area1;
+    float pib_percapita1 = (float) pib1 / populacao1;
+
     
+    printf("Primeiro Estado - População: %d \n", populacao1 );
+    printf("Primeiro Estado - Área: %0.2f \n", area1);
+    printf("Primeiro Estado - PIB: %0.2f \n", pib1);   
+    printf("Primeiro Estado - Nº Pontos Turísticos: %d \n", num_pontos_tur1);   
+    printf("Densidade Populacional do Estado 1: %0.2f \n", densidade1);
+    printf("O Pib per Capita da do Estado 1: %0.2f \n", pib_percapita1);
+
+
     printf("################################# \n");
+    
     printf("DADOS DO 2º ESTADO \n");
+
+    printf("Digite a letra do Estado: \n");
+    scanf(" %s", estado2);
+
+    printf("Digite o código da cidade: \n");
+    scanf("%s", codigo2);
 
     printf("Digite a População: \n");
     scanf("%d", &populacao2);
@@ -47,10 +69,15 @@ int main() {
     printf("Digite o Nº de Pontos Turísticos: \n");
     scanf("%d", &num_pontos_tur2);
 
+    float densidade2 = (float) populacao2 / area2;
+    float pib_percapita2 = (float) pib2 / populacao2;
+
     printf("Segundo Estado - População: %d \n", populacao2 );
-    printf("Segundo Estado - Área: %f \n", area2);
-    printf("Segundo Estado - PIB: %f \n", pib2);   
+    printf("Segundo Estado - Área: %0.2f \n", area2);
+    printf("Segundo Estado - PIB: %0.2f \n", pib2);   
     printf("Segundo Estado - Nº Pontos Turísticos: %d \n", num_pontos_tur2); 
+    printf("Densidade Populacional do Estado 2: %0.2f \n", densidade2);
+    printf("O Pib per Capita da do Estado 2: %0.2f \n",  pib_percapita2);
     
 
     return 0;
